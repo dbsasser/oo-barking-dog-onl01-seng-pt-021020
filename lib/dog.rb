2ba69ce93072321cefee9e0d25b56e6d0ba1f40f
Daniel Sasser <danielbsasser@gmail.com>
2020-02-16 00:55:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+
+class Dog 
+   def name=(dogs_name)
+     @the_dogs_name = dogs_name
+    name
+    
+    def name 
+      @the_dogs_name
+    end
+    
+    def bark
+      puts "woof!"
+    end
+    
+end
